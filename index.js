a541e119c92e543c91b9b1fb0931054cadc7112f
SandipanKarmakar04 <sandipan.sk7@gmail.com>
2024-07-31 17:11:40 +0530

    update project

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const clock = document.getElementById('clock')
-
+const day = document.getElementById('date')
 
 setInterval(()=>{
     let date = new Date()
+    let d = new Date()
     // console.log(date.toLocaleTimeString());
     clock.innerHTML = date.toLocaleTimeString();
+    day.innerHTML =d.toDateString();
 }, 1000)
